a6113e81e4f557a557ad2d213631ee71aea3e04c
Igor Kartel <igor.v.kartel@gmail.com>
2024-12-13 11:25:59 +0300

    auth unit tests

---
+++ tests/unit/conftest.py
@@ -0,0 +1,54 @@
+import pytest
+from fastapi.security import OAuth2PasswordRequestForm
+
+from dependencies.auth_dependencies import get_password_hash
+from models.user_role_enum import UserRoleEnum
+
+
+@pytest.fixture
+def unit_test_user():
+    return dict(
+        name="TestName",
+        surname="TestSurname",
+        username="test_user",
+        password="test_password",
+        email="test@example.com",
+        role=UserRoleEnum.ADMIN.value,
+    )
+
+
+@pytest.fixture
+def credentials():
+    return OAuth2PasswordRequestForm(username="test_user", password="test_password")
+
+
+@pytest.fixture
+def unit_test_user_in_db():
+    return dict(
+        id=1,
+        name="TestName",
+        surname="TestSurname",
+        username="test_user",
+        password=get_password_hash("test_password"),
+        email="test@example.com",
+        role=UserRoleEnum.ADMIN.value,
+    )
+
+
+@pytest.fixture
+def refresh_token():
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0X3VzZXIiLCJpZCI6IjEiLCJyb2xlIjoiVXNlclJvbGVFbnVtLkFETUlOIiwiZXhwIjo0MTAyNDQ0ODAwfQ.uRtDXTV1Cny_8QaDOCIRvBF18m7ZtcWCjVJDGuW7mHc"
+
+
+@pytest.fixture
+def reset_token():
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QGV4YW1wbGUuY29tIiwiZXhwIjo0MTAyNDQ0ODAwfQ.fz32glwCguoWDdSFQc3UxRIHlPG_2FE2nrB9AuB0BDk"
+
+
+@pytest.fixture
+def new_credentials():
+    return dict(
+        reset_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QGV4YW1wbGUuY29tIiwiZXhwIjo0MTAyNDQ0ODAwfQ.fz32glwCguoWDdSFQc3UxRIHlPG_2FE2nrB9AuB0BDk",
+        new_password="new_password",
+        confirm_new_password="new_password",
+    )
